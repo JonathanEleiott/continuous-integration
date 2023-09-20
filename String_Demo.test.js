0a41eb2a552633c3024c61199d387761fc30b103
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2023-09-20 10:29:29 -0400

    add basic test

--- String_Demo.test.js
+++ String_Demo.test.js
@@ -1,16 +1,19 @@
 const reverseString = require('./String_Demo');
 
 describe("Calculator tests", () => {
+  test('is true really true', () => {
+    expect(true).toBe(true);
+  });
+  
+  test('reverseString reverses a string worked correctly', () => {
+    expect(reverseString('hello')).toBe('olleh');
+  });
 
-test('reverseString reverses a string worked correctly', () => {
-  expect(reverseString('hello')).toBe('olleh');
-});
+  test('reverseString reverses a string with spaces worked correctly', () => {
+    expect(reverseString('hello world')).toBe('dlrow olleh');
+  });
 
-test('reverseString reverses a string with spaces worked correctly', () => {
-  expect(reverseString('hello world')).toBe('dlrow olleh');
-});
-
-test('reverseString reverses a string with punctuation worked correctly', () => {
-  expect(reverseString('hello, world!')).toBe('!dlrow ,olleh');
-});
+  test('reverseString reverses a string with punctuation worked correctly', () => {
+    expect(reverseString('hello, world!')).toBe('!dlrow ,olleh');
+  });
 })
