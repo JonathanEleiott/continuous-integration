b862fbf2afc02d0bf4e31d676d0a36d7b4f30a83
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2023-09-20 10:44:25 -0400

    fix loop

--- String_Demo.js
+++ String_Demo.js
@@ -1,7 +1,7 @@
 // Function to reverse a given string
 function reverseString(str) {
     let newString = "";
-    for (let i = str.length - 1; i > 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         newString += str[i];
     }
     return newString;    
